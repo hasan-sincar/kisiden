4d5ab39a1513b562bf665cfa08f5690176466a9e
hasan-sincar <hasansincar1905@gmail.com>
2025-05-20 18:45:43 +0300

    son

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 // // Initialize the Firebase app in the service worker by passing the generated config
 
 const firebaseConfig = {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    measurementId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyD_Ee9lZgqo6GHWlJ7Ps4APQDIPnPXXlIQ",
+    authDomain: "kisiden-1.firebaseapp.com",
+    projectId: "kisiden-1",
+    storageBucket: "kisiden-1.firebasestorage.app",
+    messagingSenderId: "9629773921",
+    appId: "1:9629773921:web:4c15e1d893a0b0c07f3a20",
+    measurementId: "G-BF1DNF4ZFM"
 };
 
 firebase?.initializeApp(firebaseConfig)
